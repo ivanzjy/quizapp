0f4949fc415671149b5d196b0f0db1671a2300eb
ivanzjy@gmail.com <SImple.170>
2021-02-20 15:10:38 -0800

    Model, migration and relationship - Done Create Quiz - Done

---
+++ resources/views/backend/quiz/create.blade.php
@@ -0,0 +1,52 @@
+@extends('backend.layouts.master')
+@section('title','create quiz')
+@section('content')
+    <div class="span9">
+        <div class="content">
+            <div class="module">
+                <div class="module-head">
+                    <h3>Create a quiz</h3>
+                </div>
+                <div class="module-body">
+                    <div class="control-group">
+                        <label for="" class="control-label">Quiz Name</label>
+                        <div class="controls">
+                            <input type="text" name="name" class="span8" placeholder="name of quiz"
+                                   value="{{old('name')}}">
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+
+                        <div class="controls">
+                            <label class="control-label">Quiz Description</label>
+                            <textarea type="text" name="description" class="span8" placeholder="Description"
+                                      value="{{old('description')}}"></textarea>
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+
+                        <div class="controls">
+                            <label class="control-label">Minutes</label>
+                            <input type="text" name="minutes" class="span8" placeholder="minutes"
+                                      value="{{old('minutes')}}">
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="controls">
+                        <button type="submit" class="btn btn-success">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
